9623cbfd3239b9b4f94795d9e6e09049a18fd7cd
jesper.d.svensson <jesper.d.svensson@4ad84e0a-d939-11de-971a-1339e0424b6a>
2009-11-27 11:06:34 +0000

    git-svn-id: http://sr-player.googlecode.com/svn/trunk@34 4ad84e0a-d939-11de-971a-1339e0424b6a

--- SRPlayer/src/sr/player/RightNowTask.java
+++ SRPlayer/src/sr/player/RightNowTask.java
@@ -21,7 +21,6 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.text.DateFormat;
 import java.text.ParseException;
-import java.text.ParsePosition;
 import java.util.Date;
 import java.util.Locale;
 import java.util.TimerTask;
