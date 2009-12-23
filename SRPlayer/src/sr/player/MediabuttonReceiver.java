cb3eee019295207a9a7ff91e774c885014899260
jesper.d.svensson <jesper.d.svensson@4ad84e0a-d939-11de-971a-1339e0424b6a>
2009-12-23 10:50:55 +0000

    Modified behavior to react only to PLAY and HEADSETHOOK
    git-svn-id: http://sr-player.googlecode.com/svn/trunk@75 4ad84e0a-d939-11de-971a-1339e0424b6a

--- SRPlayer/src/sr/player/MediabuttonReceiver.java
+++ SRPlayer/src/sr/player/MediabuttonReceiver.java
@@ -5,37 +5,58 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.KeyEvent;
 
 public class MediabuttonReceiver extends BroadcastReceiver{
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
-		//Check if the button was pressed or release
+		Log.v(SRPlayer.TAG, "MediabuttonReciever recived event.");
+		
+		String intentAction = intent.getAction();
+		
+		if(!Intent.ACTION_MEDIA_BUTTON.equals(intentAction)){			
+			return;
+		}
+		
 		KeyEvent MediaButtonEvent = (KeyEvent) intent.getParcelableExtra(Intent.EXTRA_KEY_EVENT);
 		
-		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
-		Boolean MediaButtonEnable = prefs.getBoolean("MediaButtonEnable", false);
-					
-		if (MediaButtonEnable)
-		{		
-			//The mediabutton should be used for play/stop
-			if ((MediaButtonEvent != null) && (MediaButtonEvent.getAction() == KeyEvent.ACTION_DOWN))
-			{	
-				
-				
-				//Mediabutton is pressed. Toggle the status of
-				//the SR Player
-				Intent ServiceIntent = new Intent(context, PlayerService.class);
-		    	ServiceIntent.addFlags(PlayerService.TOGGLE_STREAMING_STATUS);
-		    	context.startService(ServiceIntent);
-				
-			}
-			
-	    	//Do not send the broadcast to the receivers with 
-	    	//lower priority
-	    	abortBroadcast();			
+		if (MediaButtonEvent == null) {			
+            return;
+        }
+		
+        int keycode = MediaButtonEvent.getKeyCode();
+        int action = MediaButtonEvent.getAction();
+        
+        if (action == KeyEvent.ACTION_DOWN) {        	
+        	SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+    		Boolean MediaButtonEnable = prefs.getBoolean("MediaButtonEnable", false);
+    					
+    		if (MediaButtonEnable)
+    		{		
+                switch (keycode) {
 
-		}
+                case KeyEvent.KEYCODE_HEADSETHOOK:
+                case KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE:
+                	Log.v(SRPlayer.TAG, "MediabuttonReciever supported event.");
+                	Intent ServiceIntent = new Intent(context, PlayerService.class);
+    		    	ServiceIntent.addFlags(PlayerService.TOGGLE_STREAMING_STATUS);
+    		    	context.startService(ServiceIntent);
+                    break;
+                case KeyEvent.KEYCODE_MEDIA_NEXT:
+                case KeyEvent.KEYCODE_MEDIA_PREVIOUS:
+                case KeyEvent.KEYCODE_MEDIA_STOP:
+                default:
+                	Log.v(SRPlayer.TAG, "MediabuttonReciever got not yet supported media key enent.");
+                	return;
+                }
+			
+    	    	//Do not send the broadcast to the receivers with 
+    	    	//lower priority
+    	    	abortBroadcast();			
+
+    		}
+        }        
 	}
 }
