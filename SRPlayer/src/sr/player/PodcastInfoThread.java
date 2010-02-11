b8093b87d8807b0af9ade72289b6e34fb3e34be6
jesper.d.svensson <jesper.d.svensson@4ad84e0a-d939-11de-971a-1339e0424b6a>
2010-02-11 20:13:06 +0000

    Added missed header
    git-svn-id: http://sr-player.googlecode.com/svn/trunk@151 4ad84e0a-d939-11de-971a-1339e0424b6a

--- SRPlayer/src/sr/player/PodcastInfoThread.java
+++ SRPlayer/src/sr/player/PodcastInfoThread.java
@@ -1,3 +1,19 @@
+/**
+  * This file is part of SR Player for Android
+  *
+  * SR Player for Android is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License version 2 as published by
+  * the Free Software Foundation.
+  *
+  * SR Player for Android is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * You should have received a copy of the GNU General Public License
+  * along with SR Player for Android.  If not, see <http://www.gnu.org/licenses/>.
+  */
+
 package sr.player;
 
 import java.io.IOException;
